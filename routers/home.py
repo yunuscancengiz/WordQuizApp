80a5b89af67e311ae04bfeff273c99205b14e0e9
Yunus Can Cengiz <yunuscancengizz@gmail.com>
2025-07-22 05:21:12 +0300

    mock chart removed from home page... will be updated

--- routers/home.py
+++ routers/home.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from starlette.responses import RedirectResponse
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 import traceback
 from ..config import templates
-from ..models import QuizStreaks, Users, Songs
+from ..models import QuizStreaks, Users, Songs, CorrectIncorrect, Words
 from ..dependencies import db_dependency
 from ..utils.auth_utils import redirect_to_login, get_current_user
 from ..utils.theme_utils import get_theme_by_id
@@ -33,6 +33,16 @@ async def home(request: Request, db: db_dependency):
         if not song:
             song = db.query(Songs).filter(Songs.id == 1).first()
 
+
+        tomorrow = today + timedelta(days=1)
+        today_count = db.query(CorrectIncorrect).filter(
+            CorrectIncorrect.owner_id == user.get("id"),
+            CorrectIncorrect.last_attempted_at >= today,
+            CorrectIncorrect.last_attempted_at < tomorrow
+        ).count()
+
+        word_count = db.query(Words).filter(Words.owner_id == user.get("id")).count()
+
         return templates.TemplateResponse(
             "home.html",
             {
@@ -40,6 +50,8 @@ async def home(request: Request, db: db_dependency):
                 "user": user,
                 "streak": streak,
                 "max_streak": max_streak,
+                "today_question_count": today_count,
+                "total_words": word_count,
                 "theme": theme,
                 "song": song
             },
